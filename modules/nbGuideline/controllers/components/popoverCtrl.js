ed9dc1fef76bc30afffe570a4ca965343613307c
Marko Cen <markocen@gmail.com>
2017-08-28 11:33:40 -0400

    add popover page

--- modules/nbGuideline/controllers/components/popoverCtrl.js
+++ modules/nbGuideline/controllers/components/popoverCtrl.js
@@ -1,8 +1,9 @@
 (function(NetBrain){
     angular.module('nb.guideline').controller('nb.guideline.popoverCtrl', PopoverCtrl);
 
-    PopoverCtrl.$inject = [];
-    function PopoverCtrl(){
+    PopoverCtrl.$inject = ['$sce'];
+    function PopoverCtrl($sce){
         var self = this;
+        self.popoverHtml = $sce.trustAsHtml('<b style="color: red">You can</b> have customized <div class="label label-success">HTML</div> content in <code>popover</code>');
     }
 })(NetBrain)
